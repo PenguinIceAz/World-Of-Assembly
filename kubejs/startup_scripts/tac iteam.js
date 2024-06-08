c0ab58c4344b59c9d3d6544d02f9fde03c6a7e78
Gyro_XiaoL <104556835+PenguinIceAz@users.noreply.github.com>
2024-06-08 17:58:20 +0800

    回退提交未完成部分内容

--- kubejs/startup_scripts/tac iteam.js
+++
@@ -1,5 +0,0 @@
-onEvent('item.registry',event=>{
-    event.create('kubejs:incomplete_9mm','create:sequenced_assembly')
-        .texture('kubejs:item/kubejs:incomplete_9mm')
-        .displayName('装配中的9MM子弹')
-})
